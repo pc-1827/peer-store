7cfcdcf93dc887fa356ee66297b4f1ab708b801d
pc-1827 <problemcreator1827@gmail.com>
2024-09-18 16:32:49 +0530

    PathTransformFunc logic

---
+++ store.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"crypto/sha1"
+	"encoding/hex"
+	"fmt"
+	"strings"
+)
+
+func CASPathTransformFunc(key string) PathKey {
+	hash := sha1.Sum([]byte(key))
+	hashStr := hex.EncodeToString(hash[:])
+
+	blockSize := 5
+	sliceLen := len(hashStr) / blockSize
+	paths := make([]string, sliceLen)
+
+	for i := 0; i < sliceLen; i++ {
+		from, to := i*blockSize, (i*blockSize)+blockSize
+		paths[i] = hashStr[from:to]
+	}
+
+	return PathKey{
+		PathName: strings.Join(paths, "/"),
+		FileName: hashStr,
+	}
+}
+
+type PathTransformFunc func(string) PathKey
+
+type PathKey struct {
+	PathName string
+	FileName string
+}
+
+func (p PathKey) FullPathName() string {
+	return fmt.Sprintf("%s/%s", p.PathName, p.FileName)
+}
+
+func (p PathKey) FirstPathName() string {
+	paths := strings.Split(p.PathName, "/")
+	if len(paths) == 0 {
+		return ""
+	}
+	return paths[0]
+}
+
+type StoreOptions struct {
+	PathTransformFunc PathTransformFunc
+}
+
+var DefaultPathTransformFunc = func(key string) string {
+	return key
+}
+
+type Store struct {
+	StoreOptions
+}
+
+func NewStore(opts StoreOptions) *Store {
+	return &Store{
+		StoreOptions: opts,
+	}
+}
